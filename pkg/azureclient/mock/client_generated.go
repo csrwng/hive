0b67622e2162404e419b50705a834c63a3ba279d
Cesar Wong <cewong@redhat.com>
2020-08-18 18:09:50 -0400

    Add Hibernation Azure actuator

--- pkg/azureclient/mock/client_generated.go
+++ pkg/azureclient/mock/client_generated.go
@@ -6,7 +6,7 @@ package mock
 
 import (
 	context "context"
-	compute "github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2018-10-01/compute"
+	compute "github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2020-06-01/compute"
 	dns "github.com/Azure/azure-sdk-for-go/services/dns/mgmt/2018-05-01/dns"
 	gomock "github.com/golang/mock/gomock"
 	azureclient "github.com/openshift/hive/pkg/azureclient"
@@ -37,18 +37,18 @@ func (m *MockClient) EXPECT() *MockClientMockRecorder {
 }
 
 // ListResourceSKUs mocks base method
-func (m *MockClient) ListResourceSKUs(ctx context.Context) (azureclient.ResourceSKUsPage, error) {
+func (m *MockClient) ListResourceSKUs(ctx context.Context, filter string) (azureclient.ResourceSKUsPage, error) {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "ListResourceSKUs", ctx)
+	ret := m.ctrl.Call(m, "ListResourceSKUs", ctx, filter)
 	ret0, _ := ret[0].(azureclient.ResourceSKUsPage)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
 
 // ListResourceSKUs indicates an expected call of ListResourceSKUs
-func (mr *MockClientMockRecorder) ListResourceSKUs(ctx interface{}) *gomock.Call {
+func (mr *MockClientMockRecorder) ListResourceSKUs(ctx, filter interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ListResourceSKUs", reflect.TypeOf((*MockClient)(nil).ListResourceSKUs), ctx)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ListResourceSKUs", reflect.TypeOf((*MockClient)(nil).ListResourceSKUs), ctx, filter)
 }
 
 // CreateOrUpdateZone mocks base method
@@ -137,6 +137,81 @@ func (m *MockClient) DeleteRecordSet(ctx context.Context, resourceGroupName, zon
 func (mr *MockClientMockRecorder) DeleteRecordSet(ctx, resourceGroupName, zone, recordSetName, recordType interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DeleteRecordSet", reflect.TypeOf((*MockClient)(nil).DeleteRecordSet), ctx, resourceGroupName, zone, recordSetName, recordType)
+}
+
+// ListAllVirtualMachines mocks base method
+func (m *MockClient) ListAllVirtualMachines(ctx context.Context, statusOnly string) (compute.VirtualMachineListResultPage, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "ListAllVirtualMachines", ctx, statusOnly)
+	ret0, _ := ret[0].(compute.VirtualMachineListResultPage)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// ListAllVirtualMachines indicates an expected call of ListAllVirtualMachines
+func (mr *MockClientMockRecorder) ListAllVirtualMachines(ctx, statusOnly interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ListAllVirtualMachines", reflect.TypeOf((*MockClient)(nil).ListAllVirtualMachines), ctx, statusOnly)
+}
+
+// ListVirtualMachines mocks base method
+func (m *MockClient) ListVirtualMachines(ctx context.Context, resourceGroupName string) (compute.VirtualMachineListResultPage, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "ListVirtualMachines", ctx, resourceGroupName)
+	ret0, _ := ret[0].(compute.VirtualMachineListResultPage)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// ListVirtualMachines indicates an expected call of ListVirtualMachines
+func (mr *MockClientMockRecorder) ListVirtualMachines(ctx, resourceGroupName interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ListVirtualMachines", reflect.TypeOf((*MockClient)(nil).ListVirtualMachines), ctx, resourceGroupName)
+}
+
+// VirtualMachineInstanceView mocks base method
+func (m *MockClient) VirtualMachineInstanceView(ctx context.Context, resourceGroupName, machineName string) (compute.VirtualMachineInstanceView, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "VirtualMachineInstanceView", ctx, resourceGroupName, machineName)
+	ret0, _ := ret[0].(compute.VirtualMachineInstanceView)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// VirtualMachineInstanceView indicates an expected call of VirtualMachineInstanceView
+func (mr *MockClientMockRecorder) VirtualMachineInstanceView(ctx, resourceGroupName, machineName interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "VirtualMachineInstanceView", reflect.TypeOf((*MockClient)(nil).VirtualMachineInstanceView), ctx, resourceGroupName, machineName)
+}
+
+// DeallocateVirtualMachine mocks base method
+func (m *MockClient) DeallocateVirtualMachine(ctx context.Context, resourceGroup, name string) (compute.VirtualMachinesDeallocateFuture, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "DeallocateVirtualMachine", ctx, resourceGroup, name)
+	ret0, _ := ret[0].(compute.VirtualMachinesDeallocateFuture)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// DeallocateVirtualMachine indicates an expected call of DeallocateVirtualMachine
+func (mr *MockClientMockRecorder) DeallocateVirtualMachine(ctx, resourceGroup, name interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DeallocateVirtualMachine", reflect.TypeOf((*MockClient)(nil).DeallocateVirtualMachine), ctx, resourceGroup, name)
+}
+
+// StartVirtualMachine mocks base method
+func (m *MockClient) StartVirtualMachine(ctx context.Context, resourceGroup, name string) (compute.VirtualMachinesStartFuture, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "StartVirtualMachine", ctx, resourceGroup, name)
+	ret0, _ := ret[0].(compute.VirtualMachinesStartFuture)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// StartVirtualMachine indicates an expected call of StartVirtualMachine
+func (mr *MockClientMockRecorder) StartVirtualMachine(ctx, resourceGroup, name interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "StartVirtualMachine", reflect.TypeOf((*MockClient)(nil).StartVirtualMachine), ctx, resourceGroup, name)
 }
 
 // MockResourceSKUsPage is a mock of ResourceSKUsPage interface
